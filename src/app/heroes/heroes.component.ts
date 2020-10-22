41609f064ddc528de1018fc84762881cb38e4e58
maleke <malekelthomas97@gmail.com>
2020-10-22 03:00:01 -0400

    removed unused code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,18 +9,13 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService,) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
   heroes : Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero) : void {
-    this.selectedHero  = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
